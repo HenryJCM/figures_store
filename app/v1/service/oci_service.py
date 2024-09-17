a43c7ec00af7c422fed5a166c946796a66d27274
Alonso <Alonso.guicon@gmail.com>
2024-09-17 01:19:07 -0500

    configuring oci storage settings

--- app/v1/service/oci_service.py
+++ app/v1/service/oci_service.py
@@ -2,18 +2,14 @@
 import oci
 from oci.exceptions import ServiceError
 import os
-from dotenv import load_dotenv
-from pathlib import Path
-
-env_path = Path('.') / '.env'
-load_dotenv(dotenv_path=env_path)
+from app.v1.utils.config import settings
 
 
 class OCIObjectStorageService:
     def __init__(self):
         self.config = oci.config.from_file()
-        self.namespace = os.getenv('NAMESPACE_NAME')
-        self.bucket_name = os.getenv('BUCKET_NAME')
+        self.namespace = settings.namespace_name
+        self.bucket_name = settings.bucket_name
         self.client = oci.object_storage.ObjectStorageClient(self.config)
 
     def create_folder(self, folder_name: str):
